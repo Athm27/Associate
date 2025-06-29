15ffb71bd6fed6059423fbbc8ea93343f29db87e
Atharv Mehrotra <mehrotraatharv15@gmail.com>
2025-06-29 15:45:09 +0530

    Update contactController.js

--- ention-backend/controllers/contactController.js
+++ ention-backend/controllers/contactController.js
@@ -11,8 +11,8 @@ const sendContactMail = async (req, res) => {
   const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
+      user: support@entionenergy.com,
+      pass: dklldjvdgywrxnhj,
     },
   });
 
